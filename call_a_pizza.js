63b56bb3a176f647ddc0165f3d030847516f975f
Valentin <you@example.com>
2024-01-11 08:45:56 +0100

    script callapizza

---
+++ call_a_pizza.js
@@ -0,0 +1,29 @@
+//ausführen des Scripts beim Scrollen
+document.addEventListener('scroll', function(){
+      //erstellen einer Liste aller li Elemente
+const list = document.getElementsByTagName('li');
+
+//durchgehen von jeden Elemets der Liste und überprüfen ob sich der String "Pizza Hawaii" innerhalb dieses Elements befindet
+//Falls der String gefunden wurde wird das Element gelöscht. 
+
+for (let i=0; i < list.length; i++)
+    {
+        list[i]
+        const a = list[i].getElementsByTagName('a');
+        var name = a.innerhtml;
+
+        if (name.includes("Hawaii")){
+            item.remove();
+            console.log("Saved");
+        }
+
+       
+    }
+
+
+}
+
+
+
+
+)
