3b8262f349217097523005cffe4e94d2d476dbcf
Valentin <you@example.com>
2024-01-11 12:45:05 +0100

    added wop script

---
+++ worldofpizza.js
@@ -0,0 +1,22 @@
+document.addEventListener('scroll', function(){
+    const list = document.getElementsByTagName('div');
+    for(let i = 0; i < list.length; i++)
+    {
+        const item = list[i];
+        const data_testid = item.getAttribute("class");
+        if(data_testid != null){
+            //console.log(data_testid);
+            if(data_testid.includes("article-container"))
+            {
+                const inner = item.innerHTML;
+                if(inner != null){
+                    if(inner.includes("Hawaii"))
+                    {
+                        item.remove();
+                        console.log("Saved");
+                    }
+                }
+            }
+        }
+    }
+});
