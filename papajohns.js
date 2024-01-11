06ba614a635de217312987741a2bb0e3aa96507a
Schasdn <justin_lucas_mueller@outlook.de>
2024-01-11 10:35:55 +0100

    fixed papajohns

--- papajohns.js
+++ papajohns.js
@@ -1,18 +1,22 @@
 document.addEventListener('scroll', function(){
     const list = document.getElementsByTagName('div');
     for(let i = 0; i < list.length; i++)
-    { 
+    {
         const item = list[i];
         const data_testid = item.getAttribute("class");
-        if (data_testid.includes ("span article-container"))
-        {
-            var inner = data_testid.innerHTML;
-            if (inner.includes ("Hawaii"))
+        if(data_testid != null){
+            //console.log(data_testid);
+            if(data_testid.includes("span article-container"))
             {
-                item.remove();
-            console.log("Saved");
+                const inner = item.innerHTML;
+                if(inner != null){
+                    if(inner.includes("Hawaii"))
+                    {
+                        item.remove();
+                        console.log("Saved");
+                    }
+                }
             }
-            
         }
     }
 });
