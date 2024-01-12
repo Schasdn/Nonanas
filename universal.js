cf0d05d0e2ab2c6441631eb9df6aa6f6152c3404
Schasdn <justin_lucas_mueller@outlook.de>
2024-01-12 11:24:38 +0100

    added link

--- universal.js
+++ universal.js
@@ -1,14 +1,10 @@
 if(window.location.href.match("[pP]izza(.||\s)[hH]awaii")){
     console.log("LOL")
-    window.open("","_self")
+    window.open("https://nonanas.schasdn.de","_self")
 }
 else{
-    //ausführen des Scripts beim Scrollen
     document.addEventListener('scroll', function(){
-    //erstellen einer Liste aller li Elemente
     const list = document.getElementsByTagName('li');
-    //durchgehen von jeden Elemets der Liste und überprüfen ob sich der String "Pizza Hawaii" innerhalb dieses Elements befindet.
-    //Falls der String gefunden wurde wird das Element gelöscht. 
     for(let i = 0; i < list.length; i++)
     { 
         const item = list[i];
