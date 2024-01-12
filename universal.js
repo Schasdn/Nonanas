2c17ef6c5d31c37d110125c290c0a1a6f6a9259b
Schasdn <justin_lucas_mueller@outlook.de>
2024-01-12 10:28:04 +0100

    added universal + index.html

---
+++ universal.js
@@ -0,0 +1,22 @@
+if(window.location.href.match("[pP]izza(.||\s)[hH]awaii")){
+    console.log("LOL")
+    window.open("","_self")
+}
+else{
+    //ausführen des Scripts beim Scrollen
+    document.addEventListener('scroll', function(){
+    //erstellen einer Liste aller li Elemente
+    const list = document.getElementsByTagName('li');
+    //durchgehen von jeden Elemets der Liste und überprüfen ob sich der String "Pizza Hawaii" innerhalb dieses Elements befindet.
+    //Falls der String gefunden wurde wird das Element gelöscht. 
+    for(let i = 0; i < list.length; i++)
+    { 
+        const item = list[i];
+        const name = item.innerHTML;
+        if (name.includes("Pizza Hawaii")){
+            item.remove();
+            console.log("Saved");
+        }
+    }
+    });
+}
